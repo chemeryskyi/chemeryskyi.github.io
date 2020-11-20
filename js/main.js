de9c1e1c318ca7bebc92a23513e4d2fb100a22bc
chemeryskyi <e.chemeriskyi@gmail.com>
2020-11-20 14:15:26 +0200

    new page

---
+++ js/main.js
@@ -0,0 +1,54 @@
+function submitForm() {
+    event.preventDefault();
+    let formValid = true,
+        form = event.target.closest('form'),
+        btn = form.querySelector('button'),
+        inputs = form.querySelectorAll('input,textarea');
+    btn.disabled = true;
+    inputs.forEach(input => {
+        let inputValid = true,
+            line = input.closest('.popup-line');
+        if (input.dataset.type === 'text') {
+            inputValid = !!input.value.trim();
+        }
+
+        if (input.dataset.type === 'file') {
+
+            inputValid = !!input.files.length;
+        }
+        inputValid ? line.classList.remove('error') : line.classList.add('error');
+
+        formValid = inputValid && formValid;
+    });
+
+    if (formValid) {
+        console.log('submit');
+        popup.close();
+        btn.disabled = false;
+
+    } else {
+        btn.disabled = false;
+        console.log('error')
+
+    }
+}
+function fileChange() {
+    let input = event.target,
+        name = event.target.files[0].name,
+        line = input.closest('.popup-line'),
+        prevName = line.querySelector('.popup-line-comment');
+    prevName.innerHTML += `Зовантажено: ${name}`;
+    prevName.style.display = "block"
+}
+let popup = {
+    open(){
+        document.querySelector('.popup').classList.add('opened')
+    },
+    close(){
+        document.querySelector('.popup').classList.remove('opened')
+    }
+}
+
+function toggleLanguage() {
+    document.querySelector('.main-language').classList.toggle('opened')
+}
